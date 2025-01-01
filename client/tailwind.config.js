e2cc519d5d9f7584ac4e01d9831c4f276a714285
panchaliSam <panchalisam20030117@gmail.com>
2025-01-01 10:31:50 +0530

    Initial commit

---
+++ client/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html", // Entry HTML file
+    "./src/**/*.{js,ts,jsx,tsx}", // React/Vite project files
+  ],
+  theme: {
+    extend: {
+      colors: {
+        customGreen: '#38a45c', // Custom green color
+      },
+    },
+  },
+  plugins: [],
+};
