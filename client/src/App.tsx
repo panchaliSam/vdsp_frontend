274433616fdf1553d71acf52f5723aa5b1834192
panchaliSam <panchalisam20030117@gmail.com>
2025-01-05 15:33:00 +0530

    Chnage margins home

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,12 @@
 import React, { useState, useEffect } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  useLocation,
+} from "react-router-dom";
 import { Navbar, LoadingScreen, Footer } from "./components";
-import { Home } from "./pages";
+import { Home, SignIn } from "./pages";
 
 const App: React.FC = () => {
   const [loading, setLoading] = useState(true);
@@ -14,16 +20,32 @@ const App: React.FC = () => {
   }, []);
 
   return (
+    <Router>
+      <MainApp loading={loading} />
+    </Router>
+  );
+};
+
+const MainApp: React.FC<{ loading: boolean }> = ({ loading }) => {
+  const location = useLocation();
+
+  const showNavbar = location.pathname !== "/login";
+  const showFooter = location.pathname !== "/login";
+
+  return (
     <>
       {loading ? (
         <LoadingScreen />
       ) : (
         <div>
-          <Navbar />
-          <div className="my-40">
-            <Home />
-          </div>
-          <Footer />
+          {showNavbar && <Navbar />}
+
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<SignIn />} />
+          </Routes>
+
+          {showFooter && <Footer />}
         </div>
       )}
     </>
---
+++ client/src/pages/SignIn.tsx
@@ -0,0 +1,16 @@
+// import { useState } from "react";
+
+const Login = (): JSX.Element => {
+  return (
+    <div>
+      <h2 className="font-light text-center mt-10 mb-20">
+        <span className="block text-3xl sm:text-4xl md:text-5xl text-white font-[Times_New_Roman]">
+          S I G N I N
+        </span>
+        <hr className="w-[80%] mx-auto border-t border-white" />
+      </h2>
+    </div>
+  );
+};
+
+export default Login;
