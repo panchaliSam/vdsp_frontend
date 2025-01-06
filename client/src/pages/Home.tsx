41e72e069cf7c93a946457c41ca835133aff663d
panchaliSam <panchalisam20030117@gmail.com>
2025-01-06 21:40:18 +0530

    Complete navaigation home

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -170,6 +170,7 @@ const Home = (): JSX.Element => {
                 <button
                   className="w-full rounded-md bg-slate-800 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-slate-700 focus:shadow-none active:bg-slate-700 hover:bg-slate-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
                   type="button"
+                  onClick={() => navigate("/signIn")}
                 >
                   Reserve
                 </button>
