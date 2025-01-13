b369fe0cfe04fb3f39a79177efc7c2c1bb45b04b
panchaliSam <panchalisam20030117@gmail.com>
2025-01-13 20:11:20 +0530

    Implement validations

--- client/src/utils/validations/index.tsx
+++ client/src/utils/validations/index.tsx
@@ -1,3 +1,5 @@
 export { default as EmailValidation } from "./EmailValidation";
 export { default as PasswordValidation } from "./PasswordValidation ";
 export { default as ConfirmPasswordValidation } from "./ConfirmPasswordValidation ";
+export { default as NameValidation } from "./NameValidation";
+export { default as PhoneNumberValidation } from "./PhoneNumberValidation";
