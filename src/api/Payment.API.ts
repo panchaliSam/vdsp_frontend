184086e2cac6d4b554d203f2cc0d60ae9423e1e8
panchaliSam <panchalisam20030117@gmail.com>
2025-05-15 14:59:40 +0530

    Paymenet history view customer wise

--- src/api/Payment.API.ts
+++ src/api/Payment.API.ts
@@ -2,7 +2,9 @@ import axiosInstance from "@app_api/AxiosInstance";
 
 import type { PaymentRequestDto } from "@app_interfaces/Payment/PaymentRequestDto";
 import type { PaymentDto } from "@app_interfaces/Payment/PaymentDto";
+import type { PaymentHistoryDto } from "@app_interfaces/Payment/PaymentHistoryDto";
 
+//Wehen creating a reservation, we need to generate a hash for the payment
 export const generateHash = async (
   reservation: PaymentRequestDto
 ): Promise<PaymentRequestDto> => {
@@ -19,6 +21,7 @@ export const generateHash = async (
   }
 };
 
+//When the user clicks on the payment button, we need to check if the payment is already done
 export const isAlreadyPaid = async (
   reservation: PaymentDto
 ): Promise<PaymentDto> => {
@@ -46,5 +49,54 @@ export const isAlreadyPaid = async (
   } catch (error) {
     console.error("Failed to check payment status:", error);
     throw new Error("Unable to verify payment status. Please try again.");
+  }
+};
+
+//Customer Dashboard
+export const getPaymentHistory = async (): Promise<PaymentHistoryDto[]> => {
+  try {
+    const response = await axiosInstance.get<PaymentHistoryDto[]>(
+      "/paymentHistory/history"
+    )
+    return response.data;
+  } catch (error) {
+    console.error("Failed to fetch payment history:", error);
+    throw new Error("Unable to fetch payment history. Please try again.");
+  }
+}
+
+//Admin Dashboard
+export const getTotalSuccessfulPayments = async (): Promise<number> => {
+  try {
+    const response = await axiosInstance.get<number>(
+      "/paymentHistory/total-success"
+    );
+    return response.data;
+  } catch (error) {
+    console.error("Failed to fetch total payments:", error);
+    throw new Error("Unable to fetch total successful payments.");
+  }
+};
+
+//Download the report of a reservation
+export const downloadConfirmationReport = async (reservationId: number): Promise<void> => {
+  try {
+    const response = await axiosInstance.get(`/payment/reservation/${reservationId}/confirmation-letter`, {
+      responseType: 'blob',
+    });
+
+    const blob = new Blob([response.data], { type: 'application/pdf' });
+    const url = window.URL.createObjectURL(blob);
+
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = `Reservation_Confirmation_${reservationId}.pdf`;
+    document.body.appendChild(a);
+    a.click();
+    a.remove();
+    window.URL.revokeObjectURL(url);
+  } catch (error) {
+    console.error("Failed to download confirmation report:", error);
+    throw new Error("Unable to download confirmation letter.");
   }
 };
