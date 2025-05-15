6f3cd6181056305e38ada7eb063de4d12324aa11
panchaliSam <panchalisam20030117@gmail.com>
2025-05-15 16:53:41 +0530

    Chnage the dashboard section of satff

---
+++ src/pages/staff/Dashboard.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { CssBaseline } from "@mui/material";
+import DashboardLayoutBasic from "@app_components/staff/DashboardSection";
+
+export const StaffDashboard: React.FC = () => {
+  return (
+    <div>
+      <CssBaseline />
+      <DashboardLayoutBasic />
+    </div>
+  );
+};
